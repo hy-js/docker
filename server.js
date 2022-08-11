726529428443091a03e995f9208ff459d25afd5b
hy-js <rhysadawson@gmail.com>
2022-08-11 17:46:31 +1000

    add legacy version

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const express = require("express");
 const app = express();
 const port = 8080;
@@ -10,4 +12,15 @@ app.get("/", async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("good");
+  }, 300);
+  reject("bad");
+});
+
+myPromise.then(() => {
+  console.log("this will never run");
 });
